39b63c1d725ac9391bc76fe0d3a3cfd435a50e71
jeremy <jrg7148@gmail.com>
2023-12-09 14:25:15 -0600

    modified:   register.php

--- register.php
+++ register.php
@@ -28,18 +28,21 @@ try {
             //Validación que la cédula, correo y contraseña tengan el formato correcto.
             if (!preg_match('/^[1-9][0-9]*$/', $cedula_val)) {
                 $cedula_err = 'El formato de la cédula no es válido.';
-            } else if (!preg_match('/^[1-9][0-9]*$/', $telefono_val)) {
+            }
+            if (!preg_match('/^[1-9][0-9]*$/', $telefono_val)) {
                 $telefono_err = 'El formato del teléfono no es válido.';
-            } else if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i', $correo_val)) {
+            } 
+            if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i', $correo_val)) {
                 $correo_err = 'El formato del correo electrónico no es válido.';
-            } else if ((strlen($contrasena_val) < 6) || (!password_verify($confirmarContrasena_val, $contrasena_val))) {
+            } 
+            if ((strlen($contrasena_val) < 6) || (!password_verify($confirmarContrasena_val, $contrasena_val))) {
                 $confirmarContrasena_err = $contrasena_err = 'El fomato de la contraseña no es válido, recuerde que la confirmación debe ser igual y debe tener mas de seis dígitos.';
-            } else {
-                //Validación de que la cédula no esté registrada en el sistema.
-                $query = "SELECT Cédula FROM tab_usuarios WHERE Cédula = $cedula_val";
-                if (!empty(getDatosArray($query))) {
-                    $cedula_err = 'La cédula ingresada ya está registrada en el sistema.';
-                }
+            }
+            //Validación de que la cédula no esté registrada en el sistema.
+            $query = "SELECT Cédula FROM tab_usuarios WHERE Cédula = $cedula_val";
+            if (!empty(getDatosArray($query))) 
+            {
+                $cedula_err = 'La cédula ingresada ya está registrada en el sistema.';
             }
         }
 
@@ -67,49 +70,57 @@ try {
         <div class="contenedor__form">
             <img class="form__logo" src="img/logo.png" alt="Logotipo">
             <h2>Registro de usuarios</h2>
-            <p>Ingrese los siguientes datos acorde a su información.</p>
             <form class="form__datos" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                 <div class="contenedor__form__grid">
+                    <?php if($cedula_err!==''){echo "<span class='errores'>$cedula_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="number" name="cedula" id="cedula">
                         <span></span>
                         <label for="cedula">Cédula usuario:</label>
                     </div>
+                    <?php if($primerNombre_err!==''){echo "<span class='errores'>$primerNombre_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="text" name="primerNombre" id="primerNombre">
                         <span></span>
                         <label for="primerNombre">Primer nombre:</label>
                     </div>
+                    <?php if($segundoNombre_err!==''){echo "<span class='errores'>$segundoNombre_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="text" name="segundoNombre" id="segundoNombre">
                         <span></span>
                         <label for="segundoNombre">Segundo nombre:</label>
                     </div>
+                    <?php if($apellidoPaterno_err!==''){echo "<span class='errores'>$apellidoPaterno_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="text" name="primerApellido" id="primerApellido">
                         <span></span>
                         <label for="primerApellido">Primer apellido:</label>
                     </div>
+                    <?php if($apellidoMaterno_err!==''){echo "<span class='errores'>$apellidoMaterno_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="text" name="segundoApellido" id="segundoApellido">
                         <span></span>
                         <label for="segundoApellido">Segundo apellido:</label>
                     </div>
+                    <?php if($correo_err!==''){echo "<span class='errores'>$correo_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="email" name="correo" id="correo">
                         <span></span>
                         <label for="correo">Correo usuario:</label>
                     </div>
+                    <?php if($telefono_err!==''){echo "<span class='errores'>$telefono_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="number" name="telefono" id="telefono">
                         <span></span>
                         <label for="telefono">Teléfono usuario:</label>
                     </div>
+                    <?php if($confirmarContrasena_err!==''){echo "<span class='errores'>$confirmarContrasena_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="password" name="contrasena" id="contrasena">
                         <span></span>
                         <label for="contrasena">Contraseña:</label>
                     </div>
+                    <?php if($confirmarContrasena_err!==''){echo "<span class='errores'>$confirmarContrasena_err</span>";} ?>
                     <div class="usuario__data">
                         <input type="password" name="confirmarContrasena" id="confirmarContrasena">
                         <span></span>
